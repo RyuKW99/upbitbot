7e11bbeaa34ac4017f55fa4459fe81751de26be2
RyuKW99 <fbruddhks1@naver.com>
2022-02-05 21:58:27 +0900

    asdddd

--- Upbit_api_4_upload.py
+++ Upbit_api_4_upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 ############################### 모듈 import #####################################
@@ -21,18 +21,26 @@ import datetime
 
 
 ############################### 프로그램 상수 #####################################
-access_key = ""???""
-secret_key = "???"
-myToken = "?"
+access_key = "111"
+secret_key = "111"
+myToken = "11"
 
 #투자금액
-invest_money = 300000
+invest_money = 450000
 
 #거래할 코인
-krw_tickers = ['KRW-BTC', 'KRW-ETH', 'KRW-SAND', 'KRW-BORA', 'KRW-XRP','KRW-PLA','KRW-BTT','KRW-MLK','KRW-MANA','KRW-EOS'
-              ,'KRW-HIVE','KRW-WAXP','KRW-ADA', 'KRW-AQT', 'KRW-QTUM','KRW-WAVES','KRW-BAT','KRW-ETC','KRW-LINK','KRW-DOT'
-              , 'KRW-VET','KRW-STORJ','KRW-LSK','KRW-XLM','KRW-POWR','KRW-ICX','KRW-ATOM','KRW-MTL','KRW-OMG','KRW-LTC','KRW-BCH'
-              ,'KRW-BTG','KRW-NEO','KRW-ARK','KRW-IOTA', 'KRW-GLM', 'KRW-STEEM','KRW-GRS']
+krw_tickers = ['KRW-BTC', 'KRW-ETH', 'KRW-NEO', 'KRW-MTL', 'KRW-LTC', 'KRW-XRP', 'KRW-ETC', 'KRW-OMG','KRW-SNT','KRW-WAVES', 
+               'KRW-XEM', 'KRW-QTUM', 'KRW-LSK', 'KRW-STEEM', 'KRW-XLM', 'KRW-ARDR', 'KRW-ARK', 'KRW-STORJ','KRW-GRS', 'KRW-REP', 
+               'KRW-ADA', 'KRW-SBD', 'KRW-POWR', 'KRW-BTG', 'KRW-ICX', 'KRW-EOS', 'KRW-TRX', 'KRW-SC', 'KRW-ONT', 'KRW-ZIL',
+               'KRW-POLY', 'KRW-ZRX', 'KRW-LOOM', 'KRW-BCH', 'KRW-BAT', 'KRW-IOST', 'KRW-RFR', 'KRW-CVC', 'KRW-IQ', 'KRW-IOTA', 
+               'KRW-MFT', 'KRW-ONG', 'KRW-GAS', 'KRW-UPP', 'KRW-ELF', 'KRW-KNC', 'KRW-BSV', 'KRW-THETA', 'KRW-QKC', 'KRW-BTT', 
+               'KRW-MOC', 'KRW-ENJ', 'KRW-TFUEL', 'KRW-MANA', 'KRW-ANKR', 'KRW-AERGO', 'KRW-ATOM', 'KRW-TT', 'KRW-CRE', 'KRW-MBL',
+               'KRW-WAXP', 'KRW-HBAR', 'KRW-MED', 'KRW-MLK', 'KRW-STPT', 'KRW-ORBS', 'KRW-VET', 'KRW-CHZ', 'KRW-STMX', 'KRW-DKA',
+               'KRW-HIVE', 'KRW-KAVA', 'KRW-AHT', 'KRW-LINK', 'KRW-XTZ', 'KRW-BORA', 'KRW-JST', 'KRW-CRO', 'KRW-TON', 'KRW-SXP',
+               'KRW-HUNT', 'KRW-PLA', 'KRW-DOT', 'KRW-SRM','KRW-MVL','KRW-STRAX','KRW-AQT','KRW-GLM','KRW-SSX','KRW-META',
+               'KRW-FCT2','KRW-CBK','KRW-SAND','KRW-HUM','KRW-DOGE','KRW-STRK','KRW-PUNDIX','KRW-FLOW','KRW-DAWN','KRW-AXS', 
+               'KRW-STX','KRW-XEC']
+
 
 #익절,손절 퍼센트
 goodsell_percent = 1.07
@@ -309,50 +317,53 @@ buy_list = []
 
 while True:
     sell_list = []
-    
     try:
-        
         #매수조건 테스트 및 매수된 종목들 sell_list에 넣는 작업
+        my_blance = get_my_KRW_Balance()
         for symbol in krw_tickers:
             #최근 체결된 코인 정렬
-            
             order_done = upbit.get_order(symbol, state="cancel") +upbit.get_order(symbol, state="done") 
             if ( [] != order_done): #빈 배열이 있을때 오류가 나지않게하기위해서
                 order_done_sorted = sorted(order_done, key=(lambda x: x['created_at']), reverse=True)
-            
+                
+            if(order_done == []):
+                order_done_sorted = []
+
             #매수 조건 만족 시 
-            #0 코인명, 1 매수 조건 만족 했을 때 가격, 2 매수 조건이 왔을 때 +12시간
-            if(order_done_sorted[0]['side'] == 'ask' or order_done ==[]):
-                if(buy_overlap_test(symbol,buy_list) and buy_test(symbol)):  #buy overlap test를 해준 후 buy_test 실행
-                    current_price = pyupbit.get_current_price(symbol)
-                    current_time = datetime.datetime.now() 
-                    twelve_hour_later = current_time + datetime.timedelta(hours=6) 
+            #0 코인명, 1 매수 조건 만족 했을 때 가격, 2 매수 조건이 왔을 때 +6시간
+            if(order_done ==[] or order_done_sorted[0]['side'] == 'ask' ):
+                if(my_blance>invest_money):
                     
-                    temp_buy_list = []
-                    temp_buy_list.append(symbol)
-                    temp_buy_list.append(current_price)
-                    temp_buy_list.append(twelve_hour_later)
-                    buy_list.append(temp_buy_list)        
-            
-            
+                    if(buy_overlap_test(symbol,buy_list) and buy_test(symbol)):  #buy overlap test를 해준 후 buy_test 실행
+                        current_price = pyupbit.get_current_price(symbol)
+                        current_time = datetime.datetime.now() 
+                        hour_later = current_time + datetime.timedelta(hours=6) 
+
+                        temp_buy_list = []
+                        temp_buy_list.append(symbol)
+                        temp_buy_list.append(current_price)
+                        temp_buy_list.append(hour_later)
+                        buy_list.append(temp_buy_list)        
             
             #최근 체결이 매수인 코인들을 temp_list에 넣고 마지막에 2차원배열 형태로 sell_list에 insert함
-            if(order_done_sorted[0]['side'] == 'bid'):
+            if(order_done_sorted != []):
+                if(order_done_sorted[0]['side'] == 'bid' and float(order_done_sorted[0]['executed_volume']) != 0):
+
+                    #구매할 때 코인가격
+
+                    coin_buy_price = int(float(order_done_sorted[0]['price'])/float(order_done_sorted[0]['executed_volume']))
+                    #코인개수
+                    coin_count = float(order_done_sorted[0]['executed_volume'])
+
+                    #0 코인명, 1 구매할때 코인가격, 2 코인개수
+                    temp_list =[]
+                    temp_list.append(order_done_sorted[0]['market'])
+                    temp_list.append(coin_buy_price)
+                    temp_list.append(coin_count)
+                    sell_list.append(temp_list)
                 
-                #구매할 때 코인가격
-                coin_buy_price = int(float(order_done_sorted[0]['price'])/float(order_done_sorted[0]['executed_volume']))
-                #코인개수
-                coin_count = float(order_done_sorted[0]['executed_volume'])
-                
-                #0 코인명, 1 구매할때 코인가격, 2 코인개수
-                temp_list =[]
-                temp_list.append(order_done_sorted[0]['market'])
-                temp_list.append(coin_buy_price)
-                temp_list.append(coin_count)
-                sell_list.append(temp_list)
-                
-        time.sleep(1)
         
+
         #매수
         #매수 조건이 온 후 12시간이 지났을 때 매수종목 제거
         # + 조정이 왔을 때 매수
@@ -395,6 +406,7 @@ while True:
                     for i in good_sell_list:
                         if(i == sell_symbol[0]):
                             overlap_test = False
+                            
                 #현재가가 매수 * goodsell_percent 보다 높아졌을 때 good_sell_list에 코인명을 추가
                 if(overlap_test and sell_symbol[1] * goodsell_percent < current_price):
                     good_sell_list.append(sell_symbol[0])
@@ -423,11 +435,11 @@ while True:
                                     if(good_sell_list[i] == sell_symbol[0]):
                                         contem = i
                                 if(contem != 999):
-                                    del buy_list[contem]
+                                    del good_sell_list[contem]
 
 
                  
-        time.sleep(5)
+        time.sleep(8)
 
     except Exception as e:
         print(e)
@@ -435,31 +447,4 @@ while True:
         time.sleep(1)
 
         
-
-
-# In[10]:
-
-
-def buy_overlap_test (symbol, buy_list):
-    test = True
-    for i in range(len(buy_list)):
-        if (symbol == buy_list[i][0]):
-            test = False
-    return test
-sell_list = []
-#0 코인명, 1 매수 조건 만족 했을 때 가격, 2 매수 조건이 왔을 때 +12시간
-temp_list =[]
-temp_list.append("asdf")
-temp_list.append(1234)
-temp_list.append(1.234)
-sell_list.append(temp_list)
-
-temp_list = []
-temp_list.append("asdfg")
-temp_list.append(12345)
-temp_list.append(1.2345)
-sell_list.append(temp_list)
-
-print(buy_overlap_test("asdfg",sell_list))
-print(sell_list)
 
